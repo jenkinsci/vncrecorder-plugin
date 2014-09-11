0f994452c72ea18c87fe4fbf227b6b146537aab7
tbaum <tbaum@dev.java.net>
2014-09-11 16:01:57 +0000

    Initial version

---
+++ src/main/java/org/jenkinsci/plugins/vnrcecorder/vncutil/VncRecorder.java
@@ -0,0 +1,37 @@
+package org.jenkinsci.plugins.vnrcecorder.vncutil;
+
+import java.io.File;
+import java.util.concurrent.Future;
+
+public class VncRecorder extends VncProcess{
+	private volatile static Future<?> recordingState;
+/*
+ * vnc2swf
+ * xorg-x11-server-Xvfb
+ * vncviewer
+ * */
+
+	public static void main(String[] args) throws Exception 
+	{
+		String targetFile = "/tmp/test2166.swf";
+		String vncServ = "ci-loadgen1:14";
+		VncRecorderCallable vn = new VncRecorderCallable(vncServ, targetFile, new File(System.getProperty("user.home"),".vnc" + File.separator + "passwd"));
+		recordingState = execServ.submit(vn);
+		Thread.sleep(15000);
+		recordingState.cancel(true);		
+	}
+
+	public Future<?> record(String vncServ, String targetFile, File vncPassw) 
+	{
+		logger.info("Recording from server: " + vncServ + " into " + targetFile);
+		VncRecorderCallable vn = new VncRecorderCallable(vncServ, targetFile,vncPassw);
+		recordingState = execServ.submit(vn);
+		return recordingState;
+	}
+	
+	public void stop() 
+	{
+		recordingState.cancel(true);
+		shutdown();
+	}
+}
---
+++ src/main/java/org/jenkinsci/plugins/vnrcecorder/vncutil/VncTerminatorCallable.java
@@ -0,0 +1,36 @@
+package org.jenkinsci.plugins.vnrcecorder.vncutil;
+
+import java.util.concurrent.Callable;
+
+
+public class VncTerminatorCallable extends VncProcess implements Callable<Integer>  {
+
+	private int pid;
+
+	public VncTerminatorCallable(int pid) {
+		this.setPid(pid);
+	}
+
+	public VncTerminatorCallable() {
+	}
+
+	public Integer call() throws Exception 
+	{
+		executeProcess(new String []{"kill", "-SIGINT", String.valueOf(pid)});
+//		executeProcess(new String []{"kill", "-SIGTERM", String.valueOf(pid)});
+		return 0;
+
+	}
+
+	public int getPid() {
+		return pid;
+	}
+
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+
+
+}
+
+
