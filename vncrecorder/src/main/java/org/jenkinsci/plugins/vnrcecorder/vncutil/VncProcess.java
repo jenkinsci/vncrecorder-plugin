c49c3f22503aedfdde05c12c4921443260a6c182
tbaum <tbaum@dev.java.net>
2014-09-16 19:31:28 +0000

    [maven-release-plugin]  copy for tag vncrecorder-1.4

---
+++ vncrecorder/src/main/java/org/jenkinsci/plugins/vnrcecorder/vncutil/VncProcess.java
@@ -0,0 +1,67 @@
+package org.jenkinsci.plugins.vnrcecorder.vncutil;
+
+import java.io.PrintStream;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+import org.apache.log4j.WriterAppender;
+
+public class VncProcess {
+
+	protected Logger logger = Logger.getLogger(VncProcess.class);
+	static protected ExecutorService execServ = Executors.newFixedThreadPool(10);
+	static protected ExecutorService terminatorServ = Executors.newFixedThreadPool(10);
+	static protected ScheduledExecutorService splitCheckerExec = Executors.newSingleThreadScheduledExecutor();	
+	static final protected String VNC2SWF_BINARY =  "vnc2swf";
+	protected Process process;
+
+	public Logger getLoggerForPrintStream(PrintStream ps)
+	{
+		PatternLayout p = new PatternLayout("%d{ISO8601} %-5p [%t] %m%n");
+		WriterAppender wa = new WriterAppender(p,ps);
+		logger.addAppender(wa);
+		return logger;
+	}
+	
+	public VncProcess() {
+		super();
+		logger.addAppender(new ConsoleAppender( new PatternLayout("%d{ISO8601} %-5p [%t] %m%n")));
+	}
+
+
+	protected Process executeProcess(String[] com) throws Exception,
+	InterruptedException {
+		logger.debug("Starting command " + Arrays.toString(com));
+		process = new ProcessBuilder( com ).start();
+		if (!com[0].equalsIgnoreCase("kill"))
+			logger.debug("PID of command " + Arrays.toString(com) + " is: " + getUnixPID(process));
+		return process;
+	}
+
+	protected int getUnixPID(Process process) throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException {  
+		if (process.getClass().getName().equals("java.lang.UNIXProcess")) {  
+			Class<? extends Process> proc = process.getClass();  
+			Field field = proc.getDeclaredField("pid");  
+			field.setAccessible(true);  
+			Object pid = field.get(process);  
+			return (Integer) pid;  
+		} else {  
+			throw new IllegalArgumentException("Not a UNIXProcess");  
+		}  
+	}
+
+
+	public static void shutdown() 
+	{
+//		execServ.shutdownNow();
+//		terminatorServ.shutdownNow();
+//		splitCheckerExec.shutdownNow();
+	}  
+
+}
---
+++ vncrecorder/src/main/java/org/jenkinsci/plugins/vnrcecorder/vncutil/VncRecorderCallable.java
@@ -0,0 +1,95 @@
+package org.jenkinsci.plugins.vnrcecorder.vncutil;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.concurrent.Callable;
+
+public class VncRecorderCallable extends VncProcess implements Callable<Integer>  {
+	private String vncServ;
+	private String targetPath;
+	private VncTerminatorCallable term = new VncTerminatorCallable();
+	private File vncPasswFile;
+
+	public VncRecorderCallable(String vncServ,String targetPath, File vncPasswFile) {
+		this.vncServ = vncServ;
+		this.targetPath = targetPath;
+		this.vncPasswFile = vncPasswFile;
+	}
+
+	public Integer call() 
+	{
+		//				String[] com = new String []{"/usr/bin/flvrec.py", "-r1","-o", targetPath, vncServ};
+		//String[] com = new String []{"vnc2swf","-o", targetPath,"-P", "/home/dimitri/.vnc/passwd_status", "-t", "swf5", "-n", "-e", "0", vncServ};
+		String[] com = new String []{"vnc2swf","-o", targetPath, "-t", "swf5", "-n", "-e", "0", vncServ};
+		if (vncPasswFile != null)
+		{
+			com = new String []{"vnc2swf","-o", targetPath,"-P", vncPasswFile.getAbsolutePath(), "-t", "swf5", "-n", "-e", "0", vncServ};
+		}
+
+		try {
+			int i = 0;
+			while(i++ < 50)
+			{
+				int rc=0;
+				Process proc = null;
+				try {
+					proc = executeProcess(com);
+					term.setPid(getUnixPID(proc));
+					rc = proc.waitFor();
+				} catch (InterruptedException e)
+				{
+					logger.info("Command: " + Arrays.toString(com) + " canceled");
+					stop();
+					return -999;
+				}
+				catch (Exception e)
+				{
+					logger.error("Command: " + Arrays.toString(com) + " failed",e);
+					terminatorServ.submit(term);
+					return -999;
+				}
+				if (rc != 0)
+				{
+					logger.error("Command: " + Arrays.toString(com) + " returned: " + rc + ", trying " + i + " from " + 50);
+					String line;
+					BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+					BufferedReader errIn = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+					while (( line = in.readLine()) != null)
+					{
+						logger.info(line);
+					}
+					while (( line = errIn.readLine()) != null)
+					{
+						logger.error(line);
+					}
+					Thread.sleep(5000);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("Error in VncRecorderCallable", e);
+		}
+		return -1;
+	}
+
+	public String getVncServ() {
+		return vncServ;
+	}
+
+	public void setVncServ(String vncServ) {
+		this.vncServ = vncServ;
+	}
+
+	public String getTargetPath() {
+		return targetPath;
+	}
+
+	public void setTargetPath(String targetPath) {
+		this.targetPath = targetPath;
+	}
+	public void stop() throws Exception {
+		term.call();
+	}
+}
